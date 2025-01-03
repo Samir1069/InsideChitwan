03db52ea955816af30c845da468402ae807e3b87
skeleton1069 <080bel066.samir@pcampus.edu.np>
2025-01-03 21:31:55 +0545

    addd

--- myapp/views.py
+++ myapp/views.py
@@ -9,22 +9,21 @@ def home(request):
     return HttpResponse(template.render())
 def login(request):
 
-    template= loader.get_template("login.html")
     
+    if request.user.is_authenticated:  # Example condition
+        return redirect('/dashboard/')
+    template= loader.get_template("login.html")
     return HttpResponse(template.render())
 def signup(request):
     if request.user.is_authenticated:  # Example condition
         return redirect('/dashboard/')
     return render(request, 'signup.html')
-
-    template= loader.get_template("signup.html")
-    return HttpResponse(template.render())
 # Create your views here.
 def logout_view(request):
     logout(request)
     return redirect("/")
 def dashboard(request):
 
-    template= loader.get_template("dashboard.html")
+    template= loader.get_template("Dashboard.html")
     
     return HttpResponse(template.render())
